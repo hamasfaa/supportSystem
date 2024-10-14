d159d9724390d67886e2ee43b0a852821426c325
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2024-10-14 22:31:24 +0700

    random answer

--- src/SupportSystem.java
+++ src/SupportSystem.java
@@ -1,16 +1,31 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class SupportSystem {
     private Responder responder;
     private Scanner scanner;
+    private Random random;
+
+    private String[] welcomeMessages = {
+            "Welcome to the Food App Support System. Type 'bye' to exit.",
+            "Hello! How can I assist you today? Type 'bye' to exit.",
+            "Hi there! Need help? Type 'bye' to exit."
+    };
+
+    private String[] goodbyeMessages = {
+            "Goodbye!",
+            "See you next time!",
+            "Take care!"
+    };
 
     public SupportSystem() {
         responder = new Responder();
         scanner = new Scanner(System.in);
+        random = new Random();
     }
 
     public void start() {
-        System.out.println("Welcome to the Food App Support System. Type 'bye' to exit.");
+        System.out.println(getRandomWelcomeMessage());
         String userInput;
 
         do {
@@ -22,6 +37,16 @@ public class SupportSystem {
             }
         } while (!userInput.equalsIgnoreCase("bye"));
 
-        System.out.println("Goodbye!");
+        System.out.println(getRandomGoodbyeMessage());
+    }
+
+    private String getRandomWelcomeMessage() {
+        int index = random.nextInt(welcomeMessages.length);
+        return welcomeMessages[index];
+    }
+
+    private String getRandomGoodbyeMessage() {
+        int index = random.nextInt(goodbyeMessages.length);
+        return goodbyeMessages[index];
     }
 }
